a1c8166b3afe8dda86b578e51cd552e72feac30e
lucas matheus <lucasmatheusc@hotmail.com>
2023-06-29 15:30:52 -0300

    Marcando em html e bootstrap e menu header

--- painel/main.php
+++ painel/main.php
@@ -48,7 +48,9 @@
 	<!--Início menu-->
 	<nav class="navbar navbar-expand-lg bg-body-tertiary">
 		<div class="container-fluid" >
-		<a class="navbar-brand" href="#">Navbar</a>
+		<a class="navbar-brand" href="#">
+      	<img src="../Img/logo.png" alt="Logo" width="100" height="60" class="d-inline-block align-text-top">
+    	</a>
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 			</button>
@@ -80,10 +82,15 @@
 					<li><hr class="dropdown-divider"></li>
 					<li><a class="dropdown-item" href="#">Something else here</a></li>
 				</ul>
+				<li class="nav-item mx-3">
+				<a class="nav-link active" aria-current="page" href="#">SAIR</a>
+				</li>
 				</li>
 				
 			</ul>
-			<a class="navbar-brand" href="#">Navbar</a>
+			<a class="navbar-brand" href="#">
+      		<img src="../Img/rbc.png" alt="Logo" width="43" height="74" class="d-inline-block align-text-top">
+    		</a>
 			</div>
 			
 			
